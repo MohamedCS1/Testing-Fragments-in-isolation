0b4f51150394b07e4f44697286114b55d4466366
Moh Dev <73759551+MohamedCS1@users.noreply.github.com>
2023-02-19 23:08:51 +0100

    create all fragments test suite

---
+++ app/src/androidTest/java/com/example/testingfragmentsinisolation/ui/movie/AllFragmentsTestSuite.kt
@@ -0,0 +1,8 @@
+package com.example.testingfragmentsinisolation.ui.movie
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(DirectorsFragmentTest::class ,MovieDetailFragmentTest::class ,StarActorsFragmentTest::class)
+class AllFragmentsTestSuite
